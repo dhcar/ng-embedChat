73f3b044d504e47beb91de7f5f7af0c4a07c13fb
David Helgeson <davidhelgeson@Davids-MacBook-Pro.local>
2015-07-13 08:16:55 -0700

    directive oembed

--- www/chat/chat.ctrl.js
+++ www/chat/chat.ctrl.js
@@ -23,6 +23,33 @@ angular.module('embedChat')
 		};
 	};
 
+	// var XMPP = require('stanza.io'); // if using browserify
+	var client = XMPP.createClient({
+	    jid: 'dh@example.com',
+	    password: 'pass',
+	    // If you have a .well-known/host-meta.json file for your
+	    // domain, the connection transport config can be skipped.
+	    transport: 'websocket',
+	    wsURL: 'wss://localhost:5269'
+	    // (or `boshURL` if using 'bosh' as the transport)
+	});
+
+	console.log(client);
+	
+	client.on('session:started', function () {
+	    client.getRoster();
+	    client.sendPresence();
+	});
+	
+	client.on('chat', function (msg) {
+	    client.sendMessage({
+	      to: msg.from,
+	      body: 'You sent: ' + msg.body
+	    });
+	});
+	
+	client.connect();
+
 	// TODO - abstract this to a directive
 	// directive would watch ng-repeat as messages came in and change
 	// 	an embed attr to true/false/finished where it would do embedding logic
