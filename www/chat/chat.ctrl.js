1bfdcbb03a1f2dea9c35c24ff402aa2e7e05bc7b
David Helgeson <davidhelgeson@Davids-MacBook-Pro.local>
2015-07-14 18:08:02 -0700

    xmpp connection works, further lib cleaning, stanza added

--- www/chat/chat.ctrl.js
+++ www/chat/chat.ctrl.js
@@ -44,43 +44,22 @@ angular.module('embedChat')
 	];
 
 	// var XMPP = require('stanza.io'); // if using browserify
-	// var client = XMPP.createClient({
-	//     jid: 'dh@example.com',
-	//     password: 'pass',
-	//     // If you have a .well-known/host-meta.json file for your
-	//     // domain, the connection transport config can be skipped.
-	//     transport: 'websocket',
-	//     wsURL: 'wss://localhost:5269'
-	//     // (or `boshURL` if using 'bosh' as the transport)
-	// });
+	var client = XMPP.createClient({
+	    jid: 'anon@anon.lance.im',
+	    transport: 'websocket',
+	    wsURL: 'wss://lance.im:5281/xmpp-websocket'
+	    // (or `boshURL` if using 'bosh' as the transport)
+	});
 
-	// console.log(client);
+	console.log(client);
 	
-	// client.on('session:started', function () {
-	//     client.getRoster();
-	//     client.sendPresence();
-	// });
+	client.on('session:started', function () {
+	    client.getRoster();
+	    client.sendPresence();
+	    window.alert('wss xmpp success');
+	    client.disconnect();
+	});
 	
-	// client.on('chat', function (msg) {
-	//     client.sendMessage({
-	//       to: msg.from,
-	//       body: 'You sent: ' + msg.body
-	//     });
-	// });
-	
-	// client.connect();
-
-	// TODO - abstract this to a directive
-	// directive would watch ng-repeat as messages came in and change
-	// 	an embed attr to true/false/finished where it would do embedding logic
-
-	// $scope.$watch('messages.length',function(){
-	// 	console.log('msg');
-	// 	$scope.$evalAsync(function(){
-	// 		$(function(){
-	//             $("a.embed").oembed();
-	//         });
-	// 	});
-	// });
+	client.connect();
 
 });
